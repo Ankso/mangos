f86c7dc5647c8a5417ff99a1d8845fe90f03339e
Ankso <ankso@reinodelaoscuridad.com>
2010-08-09 23:13:53 +0200

    Restore PvP ranks and titles.

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -791,6 +791,8 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder *holder)
     if(!pCurrChar->IsStandState() && !pCurrChar->hasUnitState(UNIT_STAT_STUNNED))
         pCurrChar->SetStandState(UNIT_STAND_STATE_STAND);
 
+    pCurrChar->UpdateHonorFields();
+
     m_playerLoading = false;
     delete holder;
 }
