119c8557da9eb7cadfc0bdd725f35cdca748c5be
Ankso <administrador.ankso@reinodelaoscuridad.com>
2011-05-02 09:48:53 +0200

    Missing changes.

--- src/shared/revision_rdlo.h
+++ src/shared/revision_rdlo.h
@@ -1,4 +1,5 @@
 #ifndef REVISION_RDLO_H
 #define REVISION_RDLO_H
-#define REVISION_RDLO "R168"
+#define REVISION_RDLO_PUBLIC "R168" // For players, only "visible" in-game changes modify this revision.
+#define REVISION_RDLO_REAL   "R168" // For Staff, all changes modify this rev.
 #endif
