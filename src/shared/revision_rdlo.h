b681b3d72354a5194c8960f6cdb4c74a55e06eb5
Ankso <administrador.ankso@reinodelaoscuridad.com>
2011-05-08 17:05:31 +0200

    updated RdlO rev.

--- src/shared/revision_rdlo.h
+++ src/shared/revision_rdlo.h
@@ -1,5 +1,5 @@
 #ifndef REVISION_RDLO_H
 #define REVISION_RDLO_H
-#define REVISION_RDLO_PUBLIC "R169" // For players, only "visible" in-game changes modify this revision.
-#define REVISION_RDLO_REAL   "R172" // For Staff, all changes modify this rev.
+const char* REVISION_RDLO_PUBLIC = "R170"; // For players, only "visible" in-game changes modify this revision.
+const char* REVISION_RDLO_REAL   = "R175"; // For Staff, all changes modify this rev.
 #endif
